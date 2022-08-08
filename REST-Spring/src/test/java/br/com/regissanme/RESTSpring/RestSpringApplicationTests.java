289a4036f654f17ddc49e98fcba09c38de65c991
regissanme <rsanme@gmail.com>
2022-08-08 10:57:25 -0300

    first code commit

---
+++ REST-Spring/src/test/java/br/com/regissanme/RESTSpring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.regissanme.RESTSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
