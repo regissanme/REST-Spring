ce3cd78f2aaf1323afce79ae9469e71816a1b972
regissanme <rsanme@gmail.com>
2022-08-09 11:35:16 -0300

    final commit for this course

--- REST-Spring/src/main/java/br/com/regissanme/RestSpringApplication.java
+++ REST-Spring/src/main/java/br/com/regissanme/RestSpringApplication.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class RestSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestSpringApplication.class, args);
 	}
 
